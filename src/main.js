c3d9eadf7b98ef5b1a51d4b11dc825c13c8fdf3a
ericmp <ericmorenopou@gmail.com>
2021-09-05 14:06:12 +0200

    adding stuff

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("works")
+    let items = document.getElementsByClassName("item");
+
+    for (let i = 0, len = items.length; i < len; i++) {
+        items[i].addEventListener('click', () => {
+            items[i].style.background = "red"
+        })
+    }
 })
+
+// todo: fer que fins que no es carregui la lletra que no aparegui res
