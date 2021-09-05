39b00f3aa617dc75b9a3c282c304e986235bd3d3
ericmp <ericmorenopou@gmail.com>
2021-09-05 18:44:04 +0200

    added game logic

--- src/main.js
+++ src/main.js
@@ -3,16 +3,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const white = "rgb(61, 65, 75)";
     const black = "rgba(19, 21, 24, 0.616)";
     let actualColor = black;
+    let actualPlayer = "x";
 
     for (const item of items) {
         item.addEventListener('click', () => {
             if (item.style.background.length == 0) {
                 item.style.background = actualColor;
                 toggleActualColor();
+
+                item.innerHTML = actualPlayer;
+                toggleActualPlayer();
             }
 
-            item.click();
-            checkEndGame();
+            if (gameEnd()) console.log("Game ended");
         });
     }
 
@@ -21,17 +24,31 @@ document.addEventListener("DOMContentLoaded", () => {
         else if (actualColor == black) actualColor = white;
     }
 
-    function checkEndGame() {
-        const item0 = document.getElementById("item-0");
-        const item1 = document.getElementById("item-1");
-        const item2 = document.getElementById("item-2");
-        const item3 = document.getElementById("item-3");
-        const item4 = document.getElementById("item-4");
-        const item5 = document.getElementById("item-5");
-        const item6 = document.getElementById("item-6");
-        const item7 = document.getElementById("item-7");
-        const item8 = document.getElementById("item-8");
+    function toggleActualPlayer() {
+        if (actualPlayer == "x") actualPlayer = "o";
+        else if (actualPlayer == "o") actualPlayer = "x";
+    }
 
-        console.log(item0);
+    function gameEnd() {
+        const item0 = document.getElementById("item-0").innerHTML;
+        const item1 = document.getElementById("item-1").innerHTML;
+        const item2 = document.getElementById("item-2").innerHTML;
+        const item3 = document.getElementById("item-3").innerHTML;
+        const item4 = document.getElementById("item-4").innerHTML;
+        const item5 = document.getElementById("item-5").innerHTML;
+        const item6 = document.getElementById("item-6").innerHTML;
+        const item7 = document.getElementById("item-7").innerHTML;
+        const item8 = document.getElementById("item-8").innerHTML;
+
+        if (item0 == "x" && item1 == "x" && item2 == "x") return true;
+        if (item3 == "x" && item4 == "x" && item5 == "x") return true;
+        if (item6 == "x" && item7 == "x" && item8 == "x") return true;
+
+        if (item0 == "x" && item3 == "x" && item6 == "x") return true;
+        if (item1 == "x" && item4 == "x" && item7 == "x") return true;
+        if (item2 == "x" && item5 == "x" && item8 == "x") return true;
+
+        if (item0 == "x" && item4 == "x" && item8 == "x") return true;
+        if (item2 == "x" && item4 == "x" && item6 == "x") return true;
     }
 });
