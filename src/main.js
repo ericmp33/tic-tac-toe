61510d0a68e345a51d116c0c6846887328fd1177
ericmp <ericmorenopou@gmail.com>
2021-09-05 16:07:03 +0200

    improving the code

--- src/main.js
+++ src/main.js
@@ -1,11 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let items = document.getElementsByClassName("item");
+    const items = document.getElementsByClassName("item");
+    const white = "rgb(61, 65, 75)";
+    const black = "rgb(33, 37, 43)";
+    let actualColor = black;
 
     for (let i = 0, len = items.length; i < len; i++) {
         items[i].addEventListener('click', () => {
-            items[i].style.background = "red"
-        })
+            if (items[i].style.background.length == 0) {
+                items[i].style.background = actualColor;
+                toggleActualColor();
+            }
+            
+            items[i].click();
+            checkEndGame();
+        });
     }
-})
 
-// todo: fer que fins que no es carregui la lletra que no aparegui res
+    function toggleActualColor() {
+        if (actualColor == white) actualColor = black;
+        else if (actualColor == black) actualColor = white;
+    }
+
+    function checkEndGame() {
+        const item0 = document.getElementById("item-0");
+        const item1 = document.getElementById("item-1");
+        const item2 = document.getElementById("item-2");
+        const item3 = document.getElementById("item-3");
+        const item4 = document.getElementById("item-4");
+        const item5 = document.getElementById("item-5");
+        const item6 = document.getElementById("item-6");
+        const item7 = document.getElementById("item-7");
+        const item8 = document.getElementById("item-8");
+
+        console.log(item0);
+    }
+});
