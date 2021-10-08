b0c0c0ee89357cc7911d4cff2247966b6405f110
ericmp <ericmorenopou@gmail.com>
2021-10-08 19:48:35 +0200

    adding query selectors

--- src/js/game-logic.js
+++ src/js/game-logic.js
@@ -9,9 +9,9 @@ var gameOutput;
 var winnerOutput;
 
 document.addEventListener("DOMContentLoaded", () => {
-    items = document.getElementsByClassName("item");
-    gameOutput = document.getElementById("game-output");
-    winnerOutput = document.getElementById("winner");
+    items = document.querySelectorAll(".item");
+    gameOutput = document.querySelector("#game-output");
+    winnerOutput = document.querySelector("#winner");
     currentColor = black;
     currentPlayer = "x";
 
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // if one of the 2 play again game elements is clicked, reset and restart game
-    const playAgain = document.getElementsByClassName("play-again");
+    const playAgain = document.querySelectorAll(".play-again");
     for (const element of playAgain) {
         element.addEventListener('click', () => {
             // reset all elements
@@ -92,9 +92,9 @@ function checkGameEnd() {
 // colorizes to green the items that made three in a row
 function colorizeGreen(arrThreeInARow) {
     [
-        document.getElementById(arrThreeInARow[2]),
-        document.getElementById(arrThreeInARow[3]),
-        document.getElementById(arrThreeInARow[4])
+        document.querySelector("#" + arrThreeInARow[2]),
+        document.querySelector("#" + arrThreeInARow[3]),
+        document.querySelector("#" + arrThreeInARow[4])
     ]
         .forEach(item => item.style.background = green);
 }
@@ -109,15 +109,15 @@ function allItemsFilled() {
 
 // returns true if there is three in a row. if true, also returns which items made it
 function trheeInARow(p) {
-    const i0 = document.getElementById("item-0");
-    const i1 = document.getElementById("item-1");
-    const i2 = document.getElementById("item-2");
-    const i3 = document.getElementById("item-3");
-    const i4 = document.getElementById("item-4");
-    const i5 = document.getElementById("item-5");
-    const i6 = document.getElementById("item-6");
-    const i7 = document.getElementById("item-7");
-    const i8 = document.getElementById("item-8");
+    const i0 = document.querySelector("#item-0");
+    const i1 = document.querySelector("#item-1");
+    const i2 = document.querySelector("#item-2");
+    const i3 = document.querySelector("#item-3");
+    const i4 = document.querySelector("#item-4");
+    const i5 = document.querySelector("#item-5");
+    const i6 = document.querySelector("#item-6");
+    const i7 = document.querySelector("#item-7");
+    const i8 = document.querySelector("#item-8");
 
     const i0Inner = i0.innerHTML;
     const i1Inner = i1.innerHTML;
