a1bcc3402af3e2b2222016e34f83eaf295baf1e3
ericmp <ericmorenopou@gmail.com>
2021-10-12 12:28:14 +0200

    threeInARow() refactored & optimized

--- src/js/game-logic.js
+++ src/js/game-logic.js
@@ -107,38 +107,25 @@ function allItemsFilled() {
     return true;
 }
 
-// returns true if there is three in a row. if true, also returns which items made it
+// returns true and which items made it if there is three in a row
 function threeInARow(p) {
-    const i0 = document.querySelector("#item-0");
-    const i1 = document.querySelector("#item-1");
-    const i2 = document.querySelector("#item-2");
-    const i3 = document.querySelector("#item-3");
-    const i4 = document.querySelector("#item-4");
-    const i5 = document.querySelector("#item-5");
-    const i6 = document.querySelector("#item-6");
-    const i7 = document.querySelector("#item-7");
-    const i8 = document.querySelector("#item-8");
+    let conditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 4, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-    const i0Inner = i0.innerHTML;
-    const i1Inner = i1.innerHTML;
-    const i2Inner = i2.innerHTML;
-    const i3Inner = i3.innerHTML;
-    const i4Inner = i4.innerHTML;
-    const i5Inner = i5.innerHTML;
-    const i6Inner = i6.innerHTML;
-    const i7Inner = i7.innerHTML;
-    const i8Inner = i8.innerHTML;
-
-    if (threeRowAux(i0Inner, i1Inner, i2Inner, p)) return [true, p, i0.id, i1.id, i2.id];
-    if (threeRowAux(i3Inner, i4Inner, i5Inner, p)) return [true, p, i3.id, i4.id, i5.id];
-    if (threeRowAux(i6Inner, i7Inner, i8Inner, p)) return [true, p, i6.id, i7.id, i8.id];
-
-    if (threeRowAux(i0Inner, i3Inner, i6Inner, p)) return [true, p, i0.id, i3.id, i6.id];
-    if (threeRowAux(i1Inner, i4Inner, i7Inner, p)) return [true, p, i1.id, i4.id, i7.id];
-    if (threeRowAux(i2Inner, i5Inner, i8Inner, p)) return [true, p, i2.id, i5.id, i8.id];
-
-    if (threeRowAux(i0Inner, i4Inner, i8Inner, p)) return [true, p, i0.id, i4.id, i8.id];
-    if (threeRowAux(i2Inner, i4Inner, i6Inner, p)) return [true, p, i2.id, i4.id, i6.id];
+    for (const con of conditions) {
+        const elements = document.querySelectorAll(`#item-${con[0]}, #item-${con[1]}, #item-${con[2]}`);
+        if (threeRowAux(elements[0].innerHTML, elements[1].innerHTML, elements[2].innerHTML, p)) {
+            return [true, p, `item-${con[0]}`, `item-${con[1]}`, `item-${con[2]}`];
+        }
+    }
 
     return false;
 }
